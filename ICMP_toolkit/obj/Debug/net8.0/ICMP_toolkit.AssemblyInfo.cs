51f3fe4e93a656c828e917e8e96f9f49fae05121
Jerome Otto <jeromeo1969@hotmail.com>
2024-12-15 09:51:41 -0700

    Update AssemblyInfo and project configuration for consistency in debug build

--- ICMP_toolkit/obj/Debug/net8.0/ICMP_toolkit.AssemblyInfo.cs
+++ ICMP_toolkit/obj/Debug/net8.0/ICMP_toolkit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICMP_toolkit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f3daa05040b907a2c3f20692d391bbaaa6a862")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+233320fcf6d702bb0bfc4809435c2b1c64e0db7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICMP_toolkit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICMP_toolkit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
